43d0075ef814a7576ef8798b469f16ad6cb805f1
Philani Ncube <ncbphi001@gmail.com>
2024-04-18 16:08:29 +0200

    Update artist page layout and styles, and fix container alignment in artists page

--- artists.ts
+++ artists.ts
@@ -14,7 +14,7 @@ const artists = [  {
     name: "Sliqe",
     slug: "sliqe",
     image: "/images/talent/sliqe.png",
-    description: "Award winning DJ and producer, Sliqe has proved to be one of the most important personnel of the current crop in the Hip Hop genre. Since his breakthrough single Do Like I Do which earned him a South African Music Award (SAMA), Sliqe has a track record that has made him one of the most sought-after DJs in the country, spear-heading the Hip Hop genre with a fresh breath that appeals to all. Sliqe has diffused himself across the South African entertainmen scene.",
+    description: "Award winning DJ and producer, Sliqe has proved to be one of the most important personnel of the current crop in the Hip Hop genre. Since his breakthrough single Do Like I Do which earned him a South African Music Award (SAMA), Sliqe has a track record that has made him one of the most sought-after DJs in the country, spear-heading the Hip Hop genre with a fresh breath that appeals to all. Sliqe has diffused himself across the South African entertainment scene.",
     socials: {
       twitter: "https://twitter.com/Sliqe",
       instagram: "https://www.instagram.com/sliqe/",
@@ -25,7 +25,7 @@ const artists = [  {
     name: "The Black Wendy",
     slug: "wendy",
     image: "/images/talent/wendy.png",
-    description: "The face and voice of Dark & Lovely 2024, Wendy Gumede is a screenwriter and actress by profession. Known as 'The Black Wendy' to many; she has worked on award-winning local blockbuster shows like Isibaya and Scandal. She is currently a screenwriter for Netflix original shows and has also served as the voice of the popular music channel TRACE. On the social media front, Wendy has had the privilege of collaborating on campaigns with some of the most reputable brands in the country; Standard Bank, Clarins, Takealot and Engen to highlight a few.",
+    description: "The face and voice of Dark & Lovely’s Relaxed Hair Maintenance Range, Wendy Gumede is a screenwriter and actress by profession. Known as “The Black Wendy” to many; she has worked on award-winning local blockbuster shows like Isibaya and Scandal. She is currently a screenwriter for Netflix original shows and has also served as the voice of the popular music channel TRACE. On the social media front, Wendy has had the privilege of collaborating on campaigns with some of the most reputable brands in the country; Standard Bank, Clarins, Takealot and Comedy Central to highlight a few.",
     socials: {
       twitter: "https://twitter.com/",
       instagram: "https://www.instagram.com/",
@@ -36,11 +36,11 @@ const artists = [  {
     name: "Si Jones",
     slug: "si-jones",
     image: "/images/talent/sijones.png",
-    description: "Born in Mthatha Eastern Cape, South Africa; Si Jones’s career began in 2012 at Rhodes University. Her undeniable passion for entertainment along with her eclectic and vibrant personality led her into joining Rhodes Music Radio (RMR), where she learnt the skills of being a DJ and an On-Air radio presenter. Si Jones's music has been featured and playlisted on various commercial radio stations such as Metro FM, Ligwalagwala FM, 947, Radio 2000, Ukhozi FM, Tru FM, YFM, Cliff Central, Massive Metro to name a few. She has MC'd and DJ'd across South Africa, and sharing stages with the likes of Black Motion, Heavy K, Oskido, Shimza, DJ Zinhle and many more other “heavyweights” in the industry. During her 3-year period at RMR, Si Jones has presented 3 radioshows and has been the content producer for 2 of those shows",
+    description: "Si Jones is a DJ, Voice Artist and Broadcaster, specialising in Dance and House music. She’s been featured and playlisted on various commercial radio stations such as, Metro FM, Ligwalagwala FM, Radio 2000, Ukhozi FM, Tru FM, 94.7, YFM, Cliff Central, Massive Metro and many more. Si has also made it to international radio stations such as Drums Radio. Her versatility in the house music genre has allowed her the ability to perform and headline on various stages across SA alongside the greatest in SA history. With over a decade representing house music as a DJ and broadcaster, Si Jones has founded a dance music platform, charity campaigns and a podcast. Find out more on her past and present on her website www.sijones.co.za",
     socials: {
-      twitter: "https://twitter.com/kele_megano",
-      instagram: "https://www.instagram.com/kele_megano",
-      facebook: "https://www.facebook.com/Kele Megano"
+      twitter: "https://twitter.com/",
+      instagram: "https://www.instagram.com/",
+      facebook: "https://www.facebook.com/"
     },
      backgroundColor: "linear-gradient(0deg, rgba(90,34,139,1) 0%, rgba(140, 20, 252,1) 100%)",
   },  {
@@ -49,9 +49,9 @@ const artists = [  {
     image: "/images/talent/jack.png",
     description: "Being in love with music his whole life, Jack Devero joined the entertainment scene back in 2004 when he became part of the Vow FM team at Wits University. He later founded Pink Diamond Productions and Lexhosanostra and started Deejaying at these events. He has since Deejayed and made scores of people across the country dance. Whether it is Hip Hop, R&B or Old Skool Kwaito and House, he will have your guests dancing. It is a passion of his and something he is good at. Let him curate a feel-good environment at your next event.",
     socials: {
-      twitter: "https://twitter.com/kele_megano",
-      instagram: "https://www.instagram.com/kele_megano",
-      facebook: "https://www.facebook.com/Kele Megano"
+      twitter: "https://twitter.com/",
+      instagram: "https://www.instagram.com/",
+      facebook: "https://www.facebook.com/"
     },
      backgroundColor: "linear-gradient(90deg, rgba(32,164,107,1) 0%, rgba(9,121,61,1) 35%)",
   },
@@ -59,7 +59,7 @@ const artists = [  {
     name: "Ntosh Madlingozi",
     slug: "ntosh",
     image: "/images/talent/ntosh.png",
-    description: "Ntosh Madlingozi is a comedian and actor with an 18-year career across stand-up comedy, television, radio, cinema and corporate MC work. From a memorable open mic stand up debut at what was 88 Club in Norwood, in 2004, he has carved out a sustainable career based on versatility and an obsessive interest in the human condition. Over the years he has featured, to great acclaim, in theatre shows such as Blacks Only, Heavyweight Comedy Jam, 99% Xhosa Comedy Festival, Cape Town International Comedy Festival, the DSTV Comedy Festival and in turn sharing stages with the likes of Chris Forrest, John Vlismas, Trevor Noah, Tom Stade (Canada), Dwayne Perkins (U.S.A), Loyiso Gola and Kagiso Lediga among others.",
+    description: "Ntosh Madlingozi is a comedian and actor with an 19-year career across standup comedy, television, radio, cinema and corporate MC work. From a memorable open mic stand up debut at what was 88 Club in Norwood, in 2004, he has carved out a sustainable career based on versatility and an obsessive interest in the human condition. Over the years Ntosh has featured, to great acclaim, in theatre shows. From Blacks Only, Heavyweight Comedy Jam, 99% Xhosa Comedy Festival, Cape Town International Comedy Festival, to the DSTV Comedy Festival. Sharing stages with the likes of Chris Forrest, John Vlismas, Trevor Noah, Tom Stade (Canada), Dwayne Perkins (U.S.A), Loyiso Gola and Kagiso Lediga among others.",
     socials: {
       twitter: "https://twitter.com/ntosh_madlingozi",
       instagram: "https://www.instagram.com/ntoshmadlongozi/",
@@ -71,7 +71,7 @@ const artists = [  {
   name: "Vafa",
   slug: 'vafa',
   image: '/images/vafa.png',
-  description: "Vafa started his comedy journey a little over 5 years ago. His brand of clean comedy is peppered with undertones that resonate across the country and continent, making him relatable to people from many walks of life. Apart from performing stand-up comedy, Vafa directs and acts in relatable sketch comedy videos amassing millions of impressions and views across TikTok, Instagram and YouTube, where he has accumulated a large social media following. Vafa is also a talented Corporate MC, with vast experience hosting gala dinners, conferences and year-end functions. He prides himself in being able to increase participant and attendee engagement. Vafa’s adaptability and wit allow him to think on his feet to ensure that events run smoothly and successfully.",
+  description: "Meet award winnning Vafa, the comedic maestro who's been rocking stages for over 5 years! His clean comedy isn't just sidesplitting, it's relatability that resonates from coast to coast and beyond. When he's not doing his thing with stand-up, Vafa's crafting sketch comedy that's taking the internet by storm. With millions of views across TikTok, Instagram, and YouTube, he's not just a comedian, he's a viral sensation! Vafa isn't just a stage star—he's your go-to guy for hosting your next big event. From gala dinners to corporate conferences, he's the master of ceremonies who's guaranteed to ramp up the fun. With his quick wit and adaptability, he'll have your audience doubled over with laughter while ensuring your event goes off without a hitch.",
   socials: {
     twitter: "https://twitter.com",
     instagram: "https://www.instagram.com/vafaafrica",
