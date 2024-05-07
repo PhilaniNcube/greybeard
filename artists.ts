aff8fb3222f7feddb266a518b11fd6fb9caf5215
Philani Ncube <ncbphi001@gmail.com>
2024-05-07 20:20:39 +0200

    Update artist page description and social media links

--- artists.ts
+++ artists.ts
@@ -91,11 +91,11 @@ const artists = [  {
     name: "Benjahmin The Faff",
     slug: "benjamin",
     image: "/images/talent/ben.webp",
-    description: "Part of an elite customisation movement in the city of Joburg, BenjahminTheFaff produces the freshest and hottest designs. From shoes to denim, anything he puts his brush to conveys uniqueness and flair. His artwork is one of one making them rare pieces of art that aren’t mass produced but rather curated for an individual’s unique style and personality. BenjahminTheFaff uses his art to illustrate Africa through his eyes. He believes that a career as an artist is possible in Africa and strives to show case this through his work and dedication to his craft. Born in Kenya and raised in South Africa, his exposure to culture on both ends has allowed him to hone in on his skill. His work spans from Converse, Jameson, J&B, Shesha, Nike and Nandos to name a few.",
+    description: "Benjahmin is part of an elite customisation movement based in the city of Johannesburg with a national and continental reach. Benjahmins Art produces the freshest, hottest and most unique designs from concept to inception. Working on a range of mediums such as shoes, denims, caps, murals, and any other canvas. Anything he puts his brush to conveys uniqueness and flair. His artwork is one of one. Making his pieces rare pieces of art that aren’t mass produced or 100% replicated. No two pieces are completely alike and that gives the work its X factor. Each piece is specifically curated for each individuals unique style and personality.",
     socials: {
-      twitter: "https://twitter.com/kele_megano",
-      instagram: "https://www.instagram.com/kele_megano",
-      facebook: "https://www.facebook.com/Kele Megano"
+      twitter: "https://twitter.com/",
+      instagram: "https://www.instagram.com/",
+      facebook: "https://www.facebook.com/"
     },
       backgroundColor: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 100%)",
   },
