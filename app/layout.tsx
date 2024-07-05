a5248ccb2bf22b4cd02b0af18bc7e8b97d1c497e
Philani Ncube <ncbphi001@gmail.com>
2024-07-05 11:05:09 +0200

    chore: Update npm dependencies to latest versions

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ import Desktop from '@/components/desktop-nav'
 import MobileNavbar from '@/components/navbar'
 import { Toaster } from '@/components/ui/toaster'
 
+import { GoogleTagManager } from '@next/third-parties/google'
+
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://greybeardpro.co.za'),
@@ -23,6 +25,7 @@ export default function RootLayout({
   return (
 			<html lang="en">
 				<body className={`${agrandir.className} bg-black text-white`}>
+          <GoogleTagManager gtmId="GTM-57WZT4L4" />
 					<Desktop />
 					<MobileNavbar />
 					{children}
